bcbd745e972ebafb82a611dfcd8dd5338faeec22
Ramon Pires da Silva <rmn.pires@gmail.com>
2017-01-18 16:56:22 -0200

    Providing attributes display_name , verbose ,log_file in  newrelic_infra::agent class
    The attributes display_name ,verbose ,log_file could not be define in
    previous version of the module. I’m following these configurations:
    https://docs.newrelic.com/docs/infrastructure/new-relic-infrastructure/c
    onfiguration/configure-infrastructure-agent#all-links

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -27,6 +27,9 @@ class newrelic_infra::agent (
   $license_key  = '',
   $package_repo_ensure  = 'present',
   $proxy = '',
+  $display_name = '',
+  $verbose = '',
+  $log_file = '',
   $custom_attributes = {},
 ) {
   # Validate license key
@@ -105,7 +108,7 @@ class newrelic_infra::agent (
   }
 
   # we use Upstart on CentOS 6 systems and derivatives, which is not the default
-  if ($::operatingsystem == 'CentOS' and $::operatingsystemmajrelease == '6') 
+  if ($::operatingsystem == 'CentOS' and $::operatingsystemmajrelease == '6')
   or ($::operatingsystem == 'Amazon') {
     service { 'newrelic-infra':
       ensure => 'running',
