97e3431ea3538f1516c986bdd3a579169a7b5cae
nomike <nomike@nomike.com>
2017-06-19 14:14:51 +0200

    Use "upstart" to manage service also on RHEL6 not only CentOS6

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -121,7 +121,7 @@ class newrelic_infra::agent (
   }
 
   # we use Upstart on CentOS 6 systems and derivatives, which is not the default
-  if ($::operatingsystem == 'CentOS' and $::operatingsystemmajrelease == '6')
+  if (($::operatingsystem == 'CentOS' or $::operatingsystem == 'RedHat')and $::operatingsystemmajrelease == '6')
   or ($::operatingsystem == 'Amazon') {
     service { 'newrelic-infra':
       ensure => 'running',
