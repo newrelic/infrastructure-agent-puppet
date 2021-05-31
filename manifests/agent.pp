187ac775c6c9bb11eebc2aefb985c0073750017a
JakubKotkowiak <52407257+JakubKotkowiak@users.noreply.github.com>
2021-05-31 14:32:15 +0200

    Revert "Amazon linux no longer does it this way."
    This reverts commit 035a63f4828942cdbad1bf134d68545596bd48a5.

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -279,7 +279,8 @@ class newrelic_infra::agent (
   }
 
   # we use Upstart on CentOS 6 systems and derivatives, which is not the default
-  if (($::operatingsystem == 'CentOS' or $::operatingsystem == 'RedHat')and $::operatingsystemmajrelease == '6') {
+  if (($::operatingsystem == 'CentOS' or $::operatingsystem == 'RedHat')and $::operatingsystemmajrelease == '6')
+  or ($::operatingsystem == 'Amazon') {
     service { 'newrelic-infra':
       ensure   => $service_ensure,
       provider => 'upstart',
