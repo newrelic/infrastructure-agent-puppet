8b262d1106a913ef17e1aaba0e41737964796bd1
zelaskov <marcin.zelasko@codetain.eu>
2021-09-01 11:50:06 +0200

    infra: add basic github workflow WIP

--- .github/actions/installation/install.sh
+++ .github/actions/installation/install.sh
@@ -4,4 +4,4 @@ VERSION=${1:-21.7.524}
 wget https://apt.puppetlabs.com/puppet6-release-focal.deb
 sudo dpkg -i puppet6-release-focal.deb
 sudo apt-get install update -y
-sudo apt-get install puppet-agent -y
+sudo apt-get install puppet
