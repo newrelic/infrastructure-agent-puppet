caabe0d0a3a777a08c6e2b0db5753888d3a5b5cc
Jakub Kotkowiak <cckotkowiak@gmail.com>
2021-09-14 14:49:47 +0200

    Add parallel instruction

--- .github/actions/installation/install.sh
+++ .github/actions/installation/install.sh
@@ -1,7 +1,7 @@
 # Install puppet
 wget https://apt.puppetlabs.com/puppet6-release-focal.deb
 sudo dpkg -i puppet6-release-focal.deb
-sudo apt-get install update -y
+sudo apt-get install update
 sudo apt-get install puppet
 
 # Install pdk
