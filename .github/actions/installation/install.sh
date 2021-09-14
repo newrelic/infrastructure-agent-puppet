a5eda16cd10d1262953599643c0d1d3a0b4581a5
Jakub Kotkowiak <cckotkowiak@gmail.com>
2021-09-14 14:05:07 +0200

    Add pdk installation

--- .github/actions/installation/install.sh
+++ .github/actions/installation/install.sh
@@ -1,7 +1,11 @@
-VERSION=${1:-21.7.524}
-
-
+# Install puppet
 wget https://apt.puppetlabs.com/puppet6-release-focal.deb
 sudo dpkg -i puppet6-release-focal.deb
 sudo apt-get install update -y
 sudo apt-get install puppet
+
+# Install pdk
+wget https://apt.puppet.com/puppet-tools-release-focal.deb
+sudo dpkg -i puppet-tools-release-focal.deb
+sudo apt-get update
+sudo apt-get install pdk
