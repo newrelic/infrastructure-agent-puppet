4c53284f4bccb431bc15358a81bf16098c146cec
Jakub Kotkowiak <cckotkowiak@gmail.com>
2021-09-15 10:36:30 +0200

    Fix convention errors in agent_spec file

--- spec/classes/agent_spec.rb
+++ spec/classes/agent_spec.rb
@@ -1,25 +1,26 @@
 require 'spec_helper'
 
 describe 'newrelic_infra::agent' do
-  let(:params) { {'license_key' => 'TEST_VALUE'} }
-  let(:node_params) { {'lsbdistcodename' => 'xenial', 'operatingsystem' => 'Ubuntu', 'operatingsystemmajrelease' => '16.04'} }
+  let(:params) { { 'license_key' => 'TEST_VALUE' } }
+  let(:node_params) { { 'lsbdistcodename' => 'xenial', 'operatingsystem' => 'Ubuntu', 'operatingsystemmajrelease' => '16.04' } }
   let(:facts) do
-    {
-        'kernel' => "Linux",
+
+  {
+        'kernel' => 'Linux',
         'os' => {
             'name'  => 'Ubuntu',
             'family' => 'Debian',
             'release' => {
-                'full' => "16.04",
-                'major' => "16.04"
+                'full' => '16.04',
+                'major' => '16.04'
             },
             'distro' => {
-                'codename' => "xenial",
-                'description' => "Ubuntu 16.04 LTS",
-                'id' => "Ubuntu",
+                'codename' => 'xenial',
+                'description' => 'Ubuntu 16.04 LTS',
+                'id' => 'Ubuntu',
                 'release' => {
-                  'full' => "16.04",
-                  'major' => "16.04"
+                  'full' => '16.04',
+                  'major' => '16.04'
                 }
             },
         }
