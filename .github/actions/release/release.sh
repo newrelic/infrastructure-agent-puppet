959db8670a5c4f008f692bc850dee0a658fa7ae2
Jakub Kotkowiak <cckotkowiak@gmail.com>
2021-09-15 11:35:42 +0200

    Add release script

---
+++ .github/actions/release/release.sh
@@ -0,0 +1,21 @@
+#Verify is forge api key is assigned, if not exit
+if [ -z "$forge_api_key" ]; then
+    echo "forge_api_key not set, exiting..."
+    exit 1
+fi
+
+#Set variables
+url='https://forgeapi.puppet.com/v3/releases'
+metadata=$(cat metadata.json)
+release_name=$(echo "$metadata" | jq -r .name)
+release_version=$(echo "$metadata" | jq -r .version)
+name="pkg/$release_name-$release_version"
+content_type='Content-Type: multipart/form-data'
+auth_header="Authorization: Bearer $FORGE_API_KEY"
+
+#Prepare module for publishing
+pdk build --force
+
+#Publish module
+curl -fsSX 'POST' -F file="$name".tar.gz -H $content_type -H auth_header "$url"
+
