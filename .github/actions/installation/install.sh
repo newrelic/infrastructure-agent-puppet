671993d0ddbd887236d8da981b9852def7ba6be3
Jakub Kotkowiak <cckotkowiak@gmail.com>
2021-09-20 11:47:47 +0200

    Fix missing pdk

--- .github/actions/installation/install.sh
+++ .github/actions/installation/install.sh
@@ -4,8 +4,3 @@ sudo dpkg -i puppet6-release-focal.deb
 sudo apt-get install update
 sudo apt-get install puppet
 
-# Install pdk
-wget https://apt.puppet.com/puppet-tools-release-focal.deb
-sudo dpkg -i puppet-tools-release-focal.deb
-sudo apt-get update
-sudo apt-get install pdk
