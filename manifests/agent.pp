63257d160033145b6d3cdb1bbfe3c7e1f39349d6
Damon Buckwalter <dbuckwalter@newrelic.com>
2022-11-03 10:44:42 -0700

    Use https URL

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -183,7 +183,7 @@ class newrelic_infra::agent (
               }
               -> exec { 'add_newrelic_repo':
                 creates => '/etc/zypp/repos.d/newrelic-infra.repo',
-                command => "/usr/bin/zypper addrepo --repo http://download.newrelic.com/infrastructure_agent/linux/zypp/sles/${::operatingsystemrelease}/x86_64/newrelic-infra.repo",
+                command => "/usr/bin/zypper addrepo --repo https://download.newrelic.com/infrastructure_agent/linux/zypp/sles/${::operatingsystemrelease}/x86_64/newrelic-infra.repo",
                 path    => ['/usr/local/sbin', '/usr/local/bin', '/sbin', '/bin', '/usr/bin'],
               }
               # work around necessary because pacakge doesn't have Zypp provider in the puppet SLES version
